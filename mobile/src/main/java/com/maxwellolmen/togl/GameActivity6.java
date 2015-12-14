d92a837c2237faa4cb6ffb1b0ca2c4f1eca04501
Sage <sager1018@gmail.com>
2015-12-13 19:58:55 -0600

    Level 6 Starter

---
+++ mobile/src/main/java/com/maxwellolmen/togl/GameActivity6.java
@@ -0,0 +1,16 @@
+package com.maxwellolmen.togl;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+import com.maxwellolmen.togl.R;
+
+public class GameActivity6 extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game6);
+    }
+
+}
